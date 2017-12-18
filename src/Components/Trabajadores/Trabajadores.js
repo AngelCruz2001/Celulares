5ca4382e214735e14cafe7a64f78cef7508199fa
AngelCruz2001 <Angi_ed01@hotmail.com>
2017-12-18 15:48:03 -0600

    Intento de check, Actualizar Listo

--- src/Components/Trabajadores/Trabajadores.js
+++ src/Components/Trabajadores/Trabajadores.js
@@ -1,23 +1,71 @@
 import React, { Component } from 'react';
 import CSS from './Trabajadores.css';
 
+var TInicial = [
+    {
+        "ID": 1,
+        "Curp": "JIFB010925MASMGRA2",
+        "Nombre": "BRENDA",
+        "Apellidos": "JIMENEZ FIGUEROA",
+        "FechaNacimiento":"2001-09-25" ,
+        "FechaAdmision":"2017-08-21" ,
+        "Sueldo": "5000",
+        "Puesto": "ATENCION AL PUBLICO" 
+    },
+    {
+        "ID": 2,
+        "Curp": "CARG010921HDGHMRA5",
+        "Nombre": "GERARDO",
+        "Apellidos": "CHAVEZ ROMAN",
+        "FechaNacimiento":"2001-09-21" ,
+        "FechaAdmision":"2016-08-18" ,
+        "Sueldo": "4500",
+        "Puesto": "REPARADOR" 
+    },
+    {
+        "ID": 3,
+        "Curp": "CUGA010714HDGRRNA1",
+        "Nombre": "ANGEL",
+        "Apellidos": "CRUZ GARCIA",
+        "FechaNacimiento":"2001-07-14" ,
+        "FechaAdmision":"2013-10-21" ,
+        "Sueldo": "5000",
+        "Puesto": "GERENTE/DUEÑO" 
 
+    },
+    {
+        "ID": 4,
+        "Curp": "GOZE001107MDWEROA1",
+        "Nombre": "ELIZABETH",
+        "Apellidos": "GONZALEZ SAUCEDO",
+        "FechaNacimiento":"2000/11-25" ,
+        "FechaAdmision":"2015-05-16" ,
+        "Sueldo": "2000",
+        "Puesto": "VENDEDOR" 
+
+    },
+    {
+        "ID": 5,
+        "Curp": "UYRT120908MIQIERA1",
+        "Nombre": "MIGUEL ANGEL",
+        "Apellidos": "SANCHEZ MORAN",
+        "FechaNacimiento":"2001-04-18" ,
+        "FechaAdmision":"2014-12-24" ,
+        "Sueldo": "7000",
+        "Puesto": "CONDUCTOR" 
+    
+    }
+ ];
 
 class Trabajadores extends Component {
-    // componentDidMount() {
-    //     ('.ckbox label').on('click', function () {
-    //         (this).parents('tr').toggleClass('selected');
-    //       });
-    //       s('.star').on('click', function () {
-    //         s(this).toggleClass('star-checked');
-    //       });
-    //     }
+
      
     state = { 
-        T: [],
+        T:TInicial,
         fun: true, 
+        CambiarAgregar:1,
         ID:5,
-    
+        Desactivado:true,
         row:
             {
             ID: null,
@@ -28,28 +76,169 @@ class Trabajadores extends Component {
             FechaAdmision: "",
             Sueldo: "",
             Puesto: "" 
-            },
+            }
       
      
      }
+     Refresh =()=>{
+         this.setState({
+             T:TInicial
+         });
+     }
 
+            
+        
     componentDidMount(){
-        this.getTrabajadores();
-
-        // ('.ckbox label').on('click', function () {
-        //             (this).parents('tr').toggleClass('selected');
-        //           });
-        //           ('.star').on('click', function () {
-        //            (this).toggleClass('star-checked');
-        //           });
+   
+  
     }
+
+    TraerCrear =()=>{
+        var fun=this.state.fun;
+        if(fun===true){
+         this.Traer();
+        }
+        if(fun==false){
+            this.Normalidad();
+         this.setState({fun:true})
+        }
+    }
+Ver =(IdCheck)=>{
+    console.log(IdCheck.target.value)
+    var T=this.state.T;
+    var Extension=T.length;
+    var Desactivado=IdCheck+1
+    
+    for (var X=IdCheck; X<Extension;X++)
+
+    if(document.getElementById('checkbox'+X).checked){
+        
+        for (var i=Desactivado; i<=Extension; i++){
+            document.getElementById('checkbox'+i).checked=false;
+            }
+
+        }
+        console.log(IdCheck,"Desactivado: "+Desactivado);
+    }
+    CambiarAgregar =()=>{
+        var fun=this.state.fun;
+        if (fun===true){
+            var ids = this.state.ID + 1;
+        var row = {
+            ID: ids,
+            Curp: this.state.row.Curp,
+            Nombre: this.state.row.Nombre,
+            Apellidos: this.state.row.Apellidos,
+            FechaNacimiento: this.state.row.FechaNacimiento,
+            FechaAdmision: this.state.row.FechaAdmision,
+            Sueldo: this.state.row.Sueldo,
+            Puesto: this.state.row.Puesto 
+        }
+          
+          var X=this.state.T.concat([row])
+          console.log(X);
+           TInicial=X;
+           this.Refresh();
+           console.log("Agrego");
+           this.setState({CambiarAgregar:1});
+        }
+        if(fun===false){
+            var IndexDatos=1;
+            var row = {
+                ID: this.state.row.ID,
+                Curp: this.state.row.Curp,
+                Nombre: this.state.row.Nombre,
+                Apellidos: this.state.row.Apellidos,
+                FechaNacimiento: this.state.row.FechaNacimiento,
+                FechaAdmision: this.state.row.FechaAdmision,
+                Sueldo: this.state.row.Sueldo,
+                Puesto: this.state.row.Puesto 
+            }
+            this.state.T[IndexDatos] = row;
+             
+               this.Refresh();
+               console.log(this.state.T);
+               console.log("Cambio");
+               this.setState({fun: true})
+               this.Normalidad();
+               this.setState({CambiarAgregar:2,Desactivado:true});
+
+        }
+        
+        
+        
+        console.log(this.state.CambiarAgregar);
+    }
+    Traer =()=>{
+        this.setState({fun: false})
+        // var  i, j, x = "";
+        //     var T=this.state.T;
+        //     for (i in T.Trabajadores) {
+        //         x += T.Trabajadores[i].Nombre +"<br/>"  this.setState({Traer:x});
+        
+        var IndexDatos=1;
+        
+            var T = this.state.T;
+             var Convertir=JSON.stringify(T);
+             var obj = JSON.parse(Convertir);
+             var IdActTra=obj[IndexDatos].ID;
+            var CurpActTra=obj[IndexDatos].Curp;
+            var NombreActTra=obj[IndexDatos].Nombre;
+            var ApellidosActTra=obj[IndexDatos].Apellidos;
+            var FechaNacimientoActTra=obj[IndexDatos].FechaNacimiento;
+            var FechaAdmisionActTra=obj[IndexDatos].FechaAdmision;
+            var SueldoActTra=obj[IndexDatos].Sueldo;
+            var PuestoActTra=obj[IndexDatos].Puesto;
+            
+            // var FechaNacimientoTraer = new date (FechaNacimientoActTra).getFullYear();
+        
+                var row = {
+        ID: IdActTra,
+        Curp: CurpActTra,
+        Nombre: NombreActTra,
+        Apellidos: ApellidosActTra,
+        FechaNacimiento: FechaNacimientoActTra,
+        FechaAdmision: FechaAdmisionActTra,
+        Sueldo: SueldoActTra,
+        Puesto: PuestoActTra }
+        this.setState({
+            row,
+            Desactivado:false,
+            fun:false
+        })
+        
+           
+            }
 
     updateForm = () =>{
         this.setState({fun: false})
+       
+    }
+    Cambiar =()=>{
+         var IndexDatos=1;
+        var row = {
+            ID: this.state.row.ID,
+            Curp: this.state.row.Curp,
+            Nombre: this.state.row.Nombre,
+            Apellidos: this.state.row.Apellidos,
+            FechaNacimiento: this.state.row.FechaNacimiento,
+            FechaAdmision: this.state.row.FechaAdmision,
+            Sueldo: this.state.row.Sueldo,
+            Puesto: this.state.row.Puesto 
+        }
+        this.state.T[IndexDatos] = row;
+         
+           this.Refresh();
+           console.log(this.state.T);
+           console.log("Cambio");
+           this.setState({fun: true})
+           this.Normalidad();
+    }
+    Checked =()=>{
+        document.form.checkbox1.checked=false;
     }
     
-    
-    Normalidad =()=>{
+    Normalidad () {
         var row = {
             ID: null,
             Curp: "",
@@ -66,109 +255,45 @@ class Trabajadores extends Component {
     }
    
     
-     getTrabajadores () {
-    
-            var T = [
-               {
-                   "ID": 1,
-                   "Curp": "JIFB010925MASMGRA2",
-                   "Nombre": "BRENDA",
-                   "Apellidos": "JIMENEZ FIGUEROA",
-                   "FechaNacimiento":"2001/09/25" ,
-                   "FechaAdmision":"2017/08/21" ,
-                   "Sueldo": "5000",
-                   "Puesto": "ATENCION AL PUBLICO" 
-               },
-               {
-                   "ID": 2,
-                   "Curp": "CARG010921HDGHMRA5",
-                   "Nombre": "GERARDO",
-                   "Apellidos": "CHAVEZ ROMAN",
-                   "FechaNacimiento":"2001/09/21" ,
-                   "FechaAdmision":"2016/08/18" ,
-                   "Sueldo": "4500",
-                   "Puesto": "REPARADOR" 
-               },
-               {
-                   "ID": 3,
-                   "Curp": "CUGA010714HDGRRNA1",
-                   "Nombre": "ANGEL",
-                   "Apellidos": "CRUZ GARCIA",
-                   "FechaNacimiento":"2001/07/14" ,
-                   "FechaAdmision":"2013/10/21" ,
-                   "Sueldo": "5000",
-                   "Puesto": "GERENTE/DUEÑO" 
-           
-               },
-               {
-                   "ID": 4,
-                   "Curp": "GOZE001107MDWEROA1",
-                   "Nombre": "ELIZABETH",
-                   "Apellidos": "GONZALEZ SAUCEDO",
-                   "FechaNacimiento":"2000/11/25" ,
-                   "FechaAdmision":"2015/05/16" ,
-                   "Sueldo": "2000",
-                   "Puesto": "VENDEDOR" 
-           
-               },
-               {
-                   "ID": 5,
-                   "Curp": "UYRT120908MIQIERA1",
-                   "Nombre": "MIGUEL ANGEL",
-                   "Apellidos": "SANCHEZ MORAN",
-                   "FechaNacimiento":"2001/04/18" ,
-                   "FechaAdmision":"2014/12/24" ,
-                   "Sueldo": "7000",
-                   "Puesto": "CONDUCTOR" 
-               
-               }
-           ]
-           this.setState({
-               T
-           })
-   }
+   
 
      Change = (event) =>{
         const row =this.state.row;
     row[event.target.name]=event.target.value;
    
     this.setState({row});
-
+    var rowVacio = {
+            ID: null,
+            Curp: "",
+            Nombre: "",
+            Apellidos: "",
+            FechaNacimiento: "",
+            FechaAdmision: "",
+            Sueldo: "",
+            Puesto: "" 
+        }
+        
+        if(row.Curp!=rowVacio.Curp){
+            if(row.Nombre!=rowVacio.Nombre){
+                if(row.Apellidos!=rowVacio.Apellidos){
+                    if(row.FechaNacimiento!=rowVacio.FechaNacimiento){
+                        if(row.FechaAdmision!=rowVacio.FechaAdmision){
+                            if(row.Sueldo!=rowVacio.Sueldo){
+                                if(row.Puesto!=rowVacio.Puesto){
+                                  this.setState({Desactivado:false})
+                                }else{this.setState({Desactivado:true})}
+                            }else{this.setState({Desactivado:true})}
+                        }else{this.setState({Desactivado:true})}
+                    }else{this.setState({Desactivado:true})}
+                }else{this.setState({Desactivado:true})}             
+            }else{this.setState({Desactivado:true})}
+        }else{this.setState({Desactivado:true})}     
+       
     
     // this.setState({row:T})
     } 
     createForm = () =>{
-    //     const Id=this.state.Id+1;
-    //     this.setState({Id})
-    //     const T=this.state.T;
-    //     const Datos=this.state.row;
-    //  var Maximo=Math.max(this.state.T.ID);
-//    var DatosAct=new Array(this.state.row);
-// var DatosAct=Array.from(this.state.row);
-    //  var DatosAct = Array.prototype.slice.apply(this.state.row);
 
-    //   this.setState({
-    //       Id:this.state.Id+1
-    //   })  
-    //   var IdAct='"ID":'+this.state.Id;
-    //   console.log(IdAct);
-    //   this.setState({
-    //     Id_Act:this.state.Id+1
-    // })  
-        // const Id_Trab1=this.state.Id_Trab+1; //5+1 (Id actual + 1)
-        // var Id_Trab=new Array(Id_Trab1);  //Se transforma el dato anterior Array
-        // this.state.Id.concat(Id_Trab); //Se concatena en el Array con el ID
-        // this.setState({
-        //     row: this.state.row.concat([this.state.Id])
-        // });
-        // console.log('Id: ',this.state.Id)
-        // console.log('6=',Id_Trab1)
-        // console.log('Array: Con "6" ',Id_Trab)
-        // this.setState({
-        //     row: this.state.row.concat([this.state.Id_Act])
-        // });
-        // T.push(this.state.row)
-        // this.setState({T})
         var ids = this.state.ID + 1;
         var row = {
             ID: ids,
@@ -180,13 +305,21 @@ class Trabajadores extends Component {
             Sueldo: this.state.row.Sueldo,
             Puesto: this.state.row.Puesto 
         }
+          
+          var X=this.state.T.concat([row])
+          console.log(X);
+           TInicial=X;
+           this.Refresh();
+           console.log("Agrego");
+        // this.setState({ 
+        //     T: this.state.T.concat({row}),
+        //      ID: ids 
+        //   });
+        //   console.log(this.state.row);
+        //    console.log(this.state.T)
 
-        this.setState({ 
-            T: this.state.T.concat([row]),
-             ID: ids 
-          });
-          console.log(this.state.row);
-           console.log(this.state.T)
+
+        //   this.Normalidad();
         }
 
      trselected = () => {}
@@ -196,7 +329,7 @@ class Trabajadores extends Component {
     }
     render() {
         let fun = this.state.fun;
-    
+        let Trabajadores=this.state.T;
         return (
 
                 <div className="container-fluid">
@@ -228,7 +361,7 @@ class Trabajadores extends Component {
                             
                             <tbody>
                               
-                            {this.state.T.map(function(t,i){
+                            {Trabajadores.map(function(t,i){
                             //  var t_Id=t.ID;
                             //  this.setState({Id_Trab:t_Id});
                              
@@ -240,11 +373,11 @@ class Trabajadores extends Component {
                                      <input type="radio" name="options"  autocomplete="off"/>
                                      <span className="glyphicon glyphicon-ok"></span>
                                  </label> */}
-                                            
+                                            {/*  */}
                                           
  											<div className="ckbox">
- 											<input type="checkbox" id={'checkbox'+t.ID}/>
-                                             <label htmlFor={ 'checkbox'+t.ID}></label>
+ 											<input type="checkbox" id={'checkbox'+t.ID} onClick={this.Ver.bind(t.ID)} />
+                                             <label htmlFor={ 'checkbox'+t.ID} ></label>
  											</div>
                                              
 									</td>
@@ -257,14 +390,14 @@ class Trabajadores extends Component {
                                     <td>{t.Curp}</td>
                                 </tr> 
                                 )
-                            })}                               
+                            }.bind(this))}                               
                                 </tbody>
                         </table>
                       
                                 <div className="text-right container">
                                 <button type="button" className="btn btn-danger" onClick={this.state.Eliminar}>Borrar</button>
                                 
-                                <button type="button" className="btn btn-success" onClick={this.updateForm.bind()} >{fun ? 'Actualizar': 'Agregar'}</button>
+                                <button type="button" className="btn btn-success" onClick={this.TraerCrear.bind()} >{fun ? 'Actualizar': 'Agregar'}</button>
                                 </div>
                                 
                         <div className="text-center">
@@ -332,7 +465,7 @@ class Trabajadores extends Component {
 
                                 <br/>
                                 <div className="text-right container">
-                                <button type="button" className="btn btn-success"  onClick={this.createForm.bind()}>{fun ? 'Agregar' : 'Actualizar'}</button>
+                                <button type="button" className="btn btn-success"  onClick={this.CambiarAgregar.bind()} disabled={this.state.Desactivado}>{fun ? 'Agregar' : 'Actualizar'}</button>
                                 <button type="button" className="btn btn-danger" onClick={this.Normalidad.bind()} >Cancelar</button>
                                 </div>
                             {/* <div className="container">
@@ -357,10 +490,12 @@ class Trabajadores extends Component {
                         <br/>
                         <br/>
                         <br/>
-                        
-      <h1>{this.state.Id}</h1>
+                      
+<br/>
+                        <br/>
+                        <br/>
+                        <br/>
                 </div>
-      
         );
     }
 } 
